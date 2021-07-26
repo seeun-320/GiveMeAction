c5e9fa4666c1ac8f4ee01d711d24a46bc4076e54
Seungsin <8050aa@naver.com>
2021-07-26 21:29:24 +0900

    add sound

--- alarm/src/pages/alarm.js
+++ alarm/src/pages/alarm.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Clock from 'react-live-clock';
 
+import ReactAudioPlayer from 'react-audio-player';
 
 import './alarm.css';
 
 
 function Alarm({location, history}) {
-    const sound = location.search.split(/[=|&]/)[1]
+    // const sound = location.search.split(/[=|&]/)[1]
     const pose = location.search.split(/[=|&]/)[3]
 
     function goTrain(){
         history.push('/training?pose='+pose)
     }
     // printTime();
+     
     return (<>
         <div>
+        <ReactAudioPlayer
+            src="https://t1.daumcdn.net/cfile/tistory/99B972335F720D1502?original"
+            autoPlay={true}
+            loop
+        />
+            
             <div id="timebox" className="alarmdiv">
                 <span id="clock">
                     <Clock format={'HH : mm'} ticking={true}/>
