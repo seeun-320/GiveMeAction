1634f916e6dd1b71dbf80cd640e5b567b0ca226d
seeun-320 <seorang0320@gmail.com>
2021-07-26 20:24:22 +0900

    try alarm sound

--- alarm/src/pages/Main.js
+++ alarm/src/pages/Main.js
@@ -1,41 +1,45 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Clock from 'react-live-clock';
 import './main.css';
 
 import getAlarmList from '../functions/getAlarmList';
 
 import Alarm from '../components/Alarm';
 
+
+
 function Main() {
-    const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
 
-    useEffect(() => {
-      // 메모를 서버에 요쳥해서 불러오는 함수
-      getAlarmList((success, memo_list) => {
-        if (success) {
-          setList(memo_list);
-          console.log(list)
-        } else {
-          alert('서버에 오류가 생겨서 메모를 가져올 수 없습니다');
-        }
-      });
-      }, []);
 
-    return (
+  useEffect(() => {
+    // 메모를 서버에 요쳥해서 불러오는 함수
+    getAlarmList((success, memo_list) => {
+      if (success) {
+        setList(memo_list);
+        console.log(list)
+      } else {
+        alert('서버에 오류가 생겨서 메모를 가져올 수 없습니다');
+      }
+    });
+  }, []);
+
+  return (
     <>
-    <div>
+      <div>
         <div className="headers">
-            <div><Link to={'/add'} className="addBtn">+</Link></div>
-            <div><Link to={'/wait'} className="addBtn">x</Link></div>
+          <div><Link to={'/add'} className="addBtn">+</Link></div>
+          <div><Link to={'/wait'} className="addBtn">x</Link></div>
         </div>
-      <div className="alarm_list">
+        <div className="alarm_list">
           {
             list.map(alarm => (
-                <Alarm key={alarm.id} id={alarm.id} time={alarm.time} isOn={alarm.isOn}/>
+              <Alarm key={alarm.id} id={alarm.id} time={alarm.time} isOn={alarm.isOn} />
             ))
           }
+        </div>
       </div>
-    </div>
     </>
   )
 }
--- alarm/src/pages/alarm.js
+++ alarm/src/pages/alarm.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect, Component } from 'react';
 import Clock from 'react-live-clock';
 
 import { Link } from 'react-router-dom';
+import ReactAudioPlayer from 'react-audio-player';
+
+
+
 
 import './alarm.css';
 
@@ -9,25 +13,44 @@ var now = new Date();                                                  // 현재
 var min = now.getMinutes();
 var hour = now.getHours();
 
+
 function Alarm() {
-   
+
+    // document.getElementById('sound').play;
+    // <ReactAudioPlayer
+    //     src="/Users/seeun/Documents/2021인도인턴/GiveMeAction/alarm/src/chicken.MP3"
+    //     autoPlay
+    //     controls
+    // />
     // printTime();
+    // useSound('/Users/seeun/Documents/2021인도인턴/GiveMeAction/school.MP3',50,0);
     return (<>
         <body>
+
+            {/* 이거 소리나는 코드 */}
+            <audio id="sound"loop autoPlay controls>
+                <source src="https://t1.daumcdn.net/cfile/tistory/99B972335F720D1502?original" />
+                <embed src="https://t1.daumcdn.net/cfile/tistory/99B972335F720D1502?original" width="320" height="45" type="audio/mpeg" />
+
+            </audio>
+
+        
+
+
             <div id="timebox" className="alarmdiv">
                 <span id="clock">
-                    <Clock format={'HH : mm'} ticking={true}/>
-                    
-                    </span><br />
+                    <Clock format={'HH : mm'} ticking={true} />
+
+                </span><br />
                 <span id="date">
-                    <Clock format={'yyyy. MM. DD dddd'}/>
-                    </span>
+                    <Clock format={'yyyy. MM. DD dddd'} />
+                </span>
             </div>
             <div id="btnbox" className="alarmdiv"><button id="Abutton" class="btn-1" type="button">start training</button></div>
 
         </body>
     </>)
-    
+
 }
 
 export default Alarm;
